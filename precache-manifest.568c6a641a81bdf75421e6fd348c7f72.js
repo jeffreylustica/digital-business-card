d334722ebf413028e131707f4916e5dc7bb9394e
jeffnew1 <jefflustica2@gmail.com>
2022-06-18 00:09:29 +0800

    Updates

---
+++ precache-manifest.568c6a641a81bdf75421e6fd348c7f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c52b9e7dd0d7c0140bce",
+    "url": "/digital-business-card/static/css/main.335925c7.chunk.css"
+  },
+  {
+    "revision": "c52b9e7dd0d7c0140bce",
+    "url": "/digital-business-card/static/js/main.c52b9e7d.chunk.js"
+  },
+  {
+    "revision": "ea1774b402de21949992",
+    "url": "/digital-business-card/static/js/1.ea1774b4.chunk.js"
+  },
+  {
+    "revision": "2ac6fa471db2f3f58c76",
+    "url": "/digital-business-card/static/js/runtime~main.2ac6fa47.js"
+  },
+  {
+    "revision": "940c60c9735313b19bb4241859268da8",
+    "url": "/digital-business-card/static/media/display-photo.940c60c9.png"
+  },
+  {
+    "revision": "a833bef5a915529b59506f2bd7cdfcce",
+    "url": "/digital-business-card/index.html"
+  }
+];
